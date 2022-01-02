071fabba2eb91ff4364215ab17ff83208f2531f2
Falence Lemungoh <falencelemungoh@gmail.com>
2022-01-02 19:53:29 +0100

    Implement main function to start the server

--- web-app/main.go
+++ web-app/main.go
@@ -11,11 +11,13 @@ type Page struct {
 	Body []byte
 }
 
+// save method
 func (p *Page) save() error {
 	f := p.Title + ".txt"
 	return ioutil.WriteFile(f, p.Body, 0600) // 0600: for permissions
 }
 
+// load page content from file
 func load(title string) (*Page, error) {
 	f := title + ".txt"
 	body, err := ioutil.ReadFile(f)
@@ -25,8 +27,16 @@ func load(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+// Render page
 func view(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/test/"):]
 	p, _ := load(title)
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func main() {
+	p := &Page{Title: "Test", Body: []byte("Welcome to the Test page")}
+	p.save()
+	http.HandleFunc("/test/", view)
+	http.ListenAndServe(":8000", nil)
 }
