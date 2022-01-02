22ec125171f90289ae4374af569d6fe6acac16c1
Falence Lemungoh <falencelemungoh@gmail.com>
2022-01-02 15:30:35 +0100

    built a static file server

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	port := flag.String("p", "8000", "port")
+	dir := flag.String("d", ".", "dir")
+	flag.Parse()
+
+	http.Handle("/", http.FileServer(http.Dir(*dir)))
+	log.Printf("Serving %s on HTTP port: %s\n", *dir, *port)
+	log.Fatal(http.ListenAndServe(":" + *port, nil))
 }
