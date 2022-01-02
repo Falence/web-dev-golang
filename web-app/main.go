08d89f3f73b14c14a948607a40f237df765b2507
Falence Lemungoh <falencelemungoh@gmail.com>
2022-01-02 19:29:09 +0100

    Implement page load function

--- web-app/main.go
+++ web-app/main.go
@@ -16,3 +16,12 @@ func (p *Page) save() error {
 	return ioutil.WriteFile(f, p.Body, 0600) // 0600: for permissions
 }
 
+func load(title string) (*Page, error) {
+	f := title + ".txt"
+	body, err := ioutil.ReadFile(f)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
